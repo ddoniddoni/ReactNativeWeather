35ba071216759366fd21482ae4510ab5c071109a
ddoniddoni <psdkei@naver.com>
2020-03-15 18:12:16 +0900

    Finish, use React-Native and make weatherApp

--- fokin-weather/App.js
+++ fokin-weather/App.js
@@ -2,30 +2,43 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "d8395d193557f6d8cb03d38b69442b17";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
 
+    this.setState({ isLoading: false, condition: weather[0].main, temp });
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: false });
+      this.getWeather(latitude, longitude);
     } catch (error) {
-      Alert.alert("Can't find you", "So sad");
+      Alert.alert("Can't find you.", "So sad");
     }
   };
-
   componentDidMount() {
     this.getLocation();
   }
-
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 }
